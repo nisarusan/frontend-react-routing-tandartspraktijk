cb95fd35d5b9c4ae60edbc4d2861cf7bd75f7ac6
Nisar <145307487+nisarusan@users.noreply.github.com>
2023-11-19 05:00:59 +0100

    Add useNavigate hooks

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,6 @@ import Cavities from './pages/cavities/Cavities.jsx';
 import Navigation from './components/navigation/./Navigation.jsx';
 import { Routes, Route } from 'react-router-dom';
 
-// Op de homepagina en appointmentspagina staat onderaan een paragraaf met het zinnetje: 'Leer hier meer over [onderwerp]'. Zorg ervoor dat het woord 'hier' klikbaar wordt en de gebruiker doorstuurt naar de juiste pagina.
-//     Zorg ervoor dat de menu-items in Navigation.jsx klikbaar worden en de gebruiker doorsturen naar de juiste pagina;
-// Zorg ervoor dat het actieve menu-item wit onderstreept wordt, door twee CSS classes te maken in het bijbehorende Navigation.css document en deze op de juiste manier door te geven aan het NavLink-component van React Router.
 //     Maak ten slotte de button op de cavitiespagina functioneel, door de volgende acties te ondernemen:
 //     Maak een eigen, externe clickHandler-functie;
 // Zorg ervoor dat deze functie wordt aangeroepen wanneer er op de button geklikt wordt;
--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -8,10 +8,12 @@ function Navigation() {
             <div className="nav-container">
                 <h4>De Tandenborstel</h4>
                 <ul>
-                    <li><NavLink to="/">Home</NavLink></li>
-                    <li><NavLink to="/gaatjes">Gaatjes</NavLink></li>
-                    <li><NavLink to="/afspraken">Afspraak maken</NavLink></li>
-                    <li><NavLink to="tanden-bleken">Tanden bleken</NavLink></li>
+                    <li><NavLink to="/"
+                                 className={({isActive}) => isActive ? 'default-active-link' : 'default-link'}>Home</NavLink>
+                    </li>
+                    <li><NavLink to="/gaatjes" className={({isActive}) => isActive ? 'default-active-link' : 'default-link'}>Gaatjes</NavLink></li>
+                    <li><NavLink to="/afspraken" className={({isActive}) => isActive ? 'default-active-link' : 'default-link'}>Afspraak maken</NavLink></li>
+                    <li><NavLink to="tanden-bleken" className={({isActive}) => isActive ? 'default-active-link' : 'default-link'}>Tanden bleken</NavLink></li>
                 </ul>
             </div>
         </nav>
