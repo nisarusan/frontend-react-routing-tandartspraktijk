d21ba02991fcf6ca1f9f63a908bf7738eb3daa88
Nisar <145307487+nisarusan@users.noreply.github.com>
2023-11-19 04:42:27 +0100

    Added Routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,26 @@ import Home from './pages/home/Home.jsx';
 import Appointments from './pages/appointments/Appointments.jsx';
 import Cavities from './pages/cavities/Cavities.jsx';
 import Navigation from './components/navigation/./Navigation.jsx';
+import { Routes, Route } from 'react-router-dom';
 
+// Op de homepagina en appointmentspagina staat onderaan een paragraaf met het zinnetje: 'Leer hier meer over [onderwerp]'. Zorg ervoor dat het woord 'hier' klikbaar wordt en de gebruiker doorstuurt naar de juiste pagina.
+//     Zorg ervoor dat de menu-items in Navigation.jsx klikbaar worden en de gebruiker doorsturen naar de juiste pagina;
+// Zorg ervoor dat het actieve menu-item wit onderstreept wordt, door twee CSS classes te maken in het bijbehorende Navigation.css document en deze op de juiste manier door te geven aan het NavLink-component van React Router.
+//     Maak ten slotte de button op de cavitiespagina functioneel, door de volgende acties te ondernemen:
+//     Maak een eigen, externe clickHandler-functie;
+// Zorg ervoor dat deze functie wordt aangeroepen wanneer er op de button geklikt wordt;
+// Zorg ervoor dat wanneer deze functie wordt aangeroepen, er "We gaan direct door naar de afspraken pagina!" wordt gelogd in de console;
+// Stuur de gebruiker daarna door naar de appointmentspagina;
 function App() {
     return (
         <>
-            <Navigation />
-            <Home />
-            <Whitening />
-            <Appointments />
-            <Cavities />
+            <Navigation/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/tanden-bleken" element={<Whitening/>}/>
+                <Route path="/afspraken" element={<Appointments/>}/>
+                <Route path="/gaatjes" element={<Cavities/>}/>
+            </Routes>
         </>
     );
 }
--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import './Navigation.css';
+import {NavLink} from "react-router-dom";
 
 function Navigation() {
-  return (
-    <nav>
-      <div className="nav-container">
-        <h4>De Tandenborstel</h4>
-
-        <ul>
-          <li>Home</li>
-          <li>Gaatjes</li>
-          <li>Afspraak maken</li>
-          <li>Tanden bleken</li>
-        </ul>
-      </div>
-    </nav>
-  );
+    return (
+        <nav>
+            <div className="nav-container">
+                <h4>De Tandenborstel</h4>
+                <ul>
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/gaatjes">Gaatjes</NavLink></li>
+                    <li><NavLink to="/afspraken">Afspraak maken</NavLink></li>
+                    <li><NavLink to="tanden-bleken">Tanden bleken</NavLink></li>
+                </ul>
+            </div>
+        </nav>
+    );
 }
 
 export default Navigation;
